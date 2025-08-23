403bbae1627bede7cd984034cee580249b654daf
rubelw <willrubel@gmail.com>
2025-08-22 19:35:34 -0500

    Fix models

--- src/OSSS/models/person_contacts.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Column, Text, Integer, Boolean, Date, DateTime, Time, Numeric, ForeignKey, UniqueConstraint, Index, text
-
-from sqlalchemy.orm import relationship
-from .base import Base, GUID
-
-
