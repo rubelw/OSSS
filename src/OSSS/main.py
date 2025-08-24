382ab197014dc233e23398258da5f4f37cc4bc82
rubelw <willrubel@gmail.com>
2025-08-24 06:05:45 -0500

    Create courses endpoint and populate with dummy data

--- src/OSSS/main.py
+++ src/OSSS/main.py
@@ -24,6 +24,8 @@ from OSSS.api.routers.admin_settings_organizations import router as organization
 from OSSS.api.routers.sis_settings_academic_terms import router as sis_academic_terms_router
 from OSSS.api.routers.sis_settings_standardized_tests import router as sis_standardized_tests_router
 from OSSS.api.routers.sis_settings_attendance_codes import router as sis_attendance_codes_router
+from OSSS.api.routers.sis_settings_catalog_courses import router as sis_catalog_courses_router
+
 
 from OSSS.api.routers.sis_settings_behavior_codes import router as sis_behavior_codes_router
 from OSSS.api.routers import activities as activities_router
@@ -61,6 +63,8 @@ def create_app() -> FastAPI:
     app.include_router(sis_academic_terms_router)
     app.include_router(sis_standardized_tests_router)
     app.include_router(sis_attendance_codes_router)
+    app.include_router(sis_catalog_courses_router)
+
 
 
     app.include_router(activities_router.router)
