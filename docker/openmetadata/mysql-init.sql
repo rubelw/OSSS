554f3a5c241927b7b977d96c3087ad28e5aac79c
rubelw <willrubel@gmail.com>
2025-09-23 15:11:21 -0500

    Elastic working with podman

---
+++ docker/openmetadata/mysql-init.sql
@@ -0,0 +1,9 @@
+-- Ensure user/db exist with mysql_native_password and correct privileges
+CREATE DATABASE IF NOT EXISTS openmetadata_db
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+CREATE USER IF NOT EXISTS 'openmetadata_user'@'%'
+  IDENTIFIED WITH mysql_native_password BY 'openmetadata_password';
+
+GRANT ALL PRIVILEGES ON openmetadata_db.* TO 'openmetadata_user'@'%';
+FLUSH PRIVILEGES;
